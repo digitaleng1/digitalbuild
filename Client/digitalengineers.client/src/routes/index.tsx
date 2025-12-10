7d39f0661f634e48458f33871d6f315d15e7c911
alexl-digitalengineers <alexldigitalengineers@gmail.com>
2025-12-10 12:49:57 +0100

    default login route

--- Client/digitalengineers.client/src/routes/index.tsx
+++ Client/digitalengineers.client/src/routes/index.tsx
@@ -1,16 +1,16 @@
-import { Route, Routes as ReactRoutes } from 'react-router';
+import { Navigate, Route, Routes as ReactRoutes } from 'react-router';
 import ProtectedRoutes from './ProtectedRoutes';
 import ErrorPages from '@/app/error';
 import Account from '@/app/account';
-import MainPage from '@/app/main/page';
 import ErrorPageNotFound from '@/app/error/404/page';
 
 export default function AppRoutes() {
     return (
         <ReactRoutes>
             {/* Public routes - MUST BE FIRST */}
-            <Route path="/" element={<MainPage />} />
-            <Route path="/main" element={<MainPage />} />
+            {/* Temporarily redirect landing to login */}
+            <Route path="/" element={<Navigate to="/account/login" replace />} />
+            <Route path="/main" element={<Navigate to="/account/login" replace />} />
             <Route path="/account/*" element={<Account />} />
             <Route path="/error/*" element={<ErrorPages />} />
             
